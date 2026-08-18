23920754fe7cf9cc1bebf2e14106aeb366988d63
takasimakakeru <kyunpangoogle@outlook.jp>
2026-08-18 01:42:50 +0000

    a

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- 1. lazy.nvim の自動インストール
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -14,6 +14,5 @@ vim.g.maplocalleader = " "
 
 -- 3. 外部設定ファイルの読み込み
 require("config.lazy")
-require("config.lsp")
 
 vim.opt.number = true
